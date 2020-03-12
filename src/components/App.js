00632171de2f34f3057a2aec774eec1860b33e73
clshu <ccss0630@yahoo.com>
2020-03-11 18:33:30 -0700

    #1 Intro. Set up skeloton App, Header, Footer, Exercises components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react'
+import React, { Fragment } from 'react'
+import { Header, Footer } from './Layout'
+import Excercises from './Excercises'
 
-class App extends Component {
-  render() {
-    return <div>App</div>
-  }
+const App = () => {
+  return (
+    <Fragment>
+      <Header />
+      <Excercises />
+      <Footer />
+    </Fragment>
+  )
 }
 
 export default App
